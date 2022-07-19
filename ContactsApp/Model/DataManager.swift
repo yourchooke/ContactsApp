5cabaf8fc03c26e81e534e2ab28740682a3e725a
Olga Yurchuk <olga.yurchuk@xored.com>
2022-07-19 22:46:09 +0700

    Data manager class is added

--- ContactsApp/Model/DataManager.swift
+++ ContactsApp/Model/DataManager.swift
@@ -6,3 +6,49 @@
 //
 
 
+class DataManager {
+    let names = [
+        "Valera",
+        "Seryoga",
+        "Johny",
+        "Shura",
+        "Lenya",
+        "Lyoha",
+        "Grisha",
+        "Pasha"
+    ]
+    
+    let surnames = [
+        "Ivanov",
+        "Suvorov",
+        "Pyatnov",
+        "Burunov",
+        "Valunov",
+        "Kruglov",
+        "Volkov",
+        "Putnov"
+    ]
+    
+    let phoneNumbers = [
+        "1234567890",
+        "1234567891",
+        "1234567892",
+        "1234567893",
+        "1234567894",
+        "1234567895",
+        "1234567896",
+        "1234567897"
+    ]
+    
+    let emails = [
+        "1a@shmail.ru",
+        "2b@shmail.ru",
+        "3c@shmail.ru",
+        "4d@shmail.ru",
+        "5e@shmail.ru",
+        "6f@shmail.ru",
+        "7g@shmail.ru",
+        "8h@shmail.ru",
+    ]
+    
+}
