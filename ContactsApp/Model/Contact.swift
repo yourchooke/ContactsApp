fd9fa51aea1956e38059dbcd737a9d51203d6de7
Olga Yurchuk <olga.yurchuk@xored.com>
2022-07-19 22:23:44 +0700

    Project hierarchy

---
+++ ContactsApp/Model/Contact.swift
@@ -0,0 +1,8 @@
+//
+//  Contact.swift
+//  ContactsApp
+//
+//  Created by Olga Yurchuk on 19.07.2022.
+//
+
+import Foundation
---
+++ ContactsApp/Model/DataManager.swift
@@ -0,0 +1,8 @@
+//
+//  DataManager.swift
+//  ContactsApp
+//
+//  Created by Olga Yurchuk on 19.07.2022.
+//
+
+
